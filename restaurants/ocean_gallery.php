d469f5b3e2a6e723fa042a50b9ece159cfd866fb
Dilum <sudheeradilum@outlook.com>
2025-07-25 07:36:50 +0530

    gallery css update

--- restaurants/ocean_gallery.php
+++ restaurants/ocean_gallery.php
@@ -13,7 +13,7 @@
 
     <link href="https://fonts.googleapis.com/css2?family=Marcellus&family=Montserrat:wght@300;400;500;600&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
-    <link rel="stylesheet" href="../assets/css/barn_gallery.css">
+    <link rel="stylesheet" href="../assets/css/ocean_gallery.css">
 
 </head>
 
