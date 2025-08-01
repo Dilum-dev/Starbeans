6297cebd1a4d76e8e935a182351271f00a754847
Dilum <sudheeradilum@outlook.com>
2025-08-01 20:36:46 +0530

    header change

--- restaurants/barn_gallery.php
+++ restaurants/barn_gallery.php
@@ -9,7 +9,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Thaproban Pavilion - Spa & Wellness</title>
+    <title>The Barn Gallery</title>
 
     <link href="https://fonts.googleapis.com/css2?family=Marcellus&family=Montserrat:wght@300;400;500;600&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
